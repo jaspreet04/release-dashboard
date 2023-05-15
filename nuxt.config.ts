a519209dde74df689192b8c290f581e6d0fdc392
jaspreet04 <dhimanjaspreet04@gmail.com>
2023-05-15 13:13:47 -0700

    update to nuxt 3

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    runtimeConfig: {
+        // The private keys which are only available server-side
+        githubSecret: process.env.GITHUB_TOKEN,
+        // Keys within public are also exposed client-side
+        public: {
+          apiBase: '/api'
+        }
+      }
+})
